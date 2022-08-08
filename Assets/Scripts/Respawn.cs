e3fa3f7c59ccc025cca1ef37a72ae7cc0118a3e0
Your Name <you@example.com>
2022-08-08 20:32:46 +0900

    Respawn_Creature

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -7,7 +7,11 @@ public class Respawn : MonoBehaviour
 {
 
     [SerializeField]
-    static int Maximum = 1;
+    static int Catridge_Big = 2;
+    [SerializeField]
+    static int Catridge_Medium = 1;
+    [SerializeField]
+    static int Catridge_Small = 1;
     [SerializeField]
     static int Count_Big = 1;
     [SerializeField]
@@ -15,9 +19,12 @@ public class Respawn : MonoBehaviour
     [SerializeField]
     static int Count_Small = 3;
 
-    public GameObject[] Object_Big = new GameObject[Maximum];
-    public GameObject[] Object_Medium = new GameObject[Maximum];
-    public GameObject[] Object_Small = new GameObject[Maximum];
+    int range_X = 0;
+    int range_Z = 0;
+
+    public GameObject[] Object_Big = new GameObject[Catridge_Big];
+    public GameObject[] Object_Medium = new GameObject[Catridge_Medium];
+    public GameObject[] Object_Small = new GameObject[Catridge_Small];
     public GameObject Object_Ground;
     BoxCollider Range_Collider;
 
@@ -28,8 +35,12 @@ public class Respawn : MonoBehaviour
         Range_Collider = Object_Ground.GetComponent<BoxCollider>();
     }
 
-    private void Start()
+    public void Start()
     {
+        range_X = (int)Range_Collider.bounds.size.x;
+        range_Z = (int)Range_Collider.bounds.size.z;
+        int[,] MAP = new int[range_Z, range_X];
+        Making_Map(MAP);
         StartCoroutine(Random_Respawn());
     }
 
@@ -37,11 +48,8 @@ public class Respawn : MonoBehaviour
     {
         Vector3 Pos = Object_Ground.transform.position;
 
-        int range_X = (int)Range_Collider.bounds.size.x;
-        int range_Z = (int)Range_Collider.bounds.size.z;
-
-        int[,] MAP = new int[range_Z,range_X];
-
+        range_X = (int)Range_Collider.bounds.size.x;
+        range_Z = (int)Range_Collider.bounds.size.z;
 
         range_X = Random.Range(((range_X / 2) * -1) + 1, (range_X / 2) - 1);
         range_Z = Random.Range(((range_Z / 2) * -1) + 1, (range_Z / 2) - 1);
@@ -57,7 +65,9 @@ public class Respawn : MonoBehaviour
         {
             //1초마다 랜덤으로 생성하도록 설정
             //추후 매페이즈 마다 생성하도록 트리거를 넣을 예정
-            int a = Random.Range(0, Maximum);
+            int a = Random.Range(0, Catridge_Big);
+            int b = Random.Range(0, Catridge_Medium);
+            int c = Random.Range(0, Catridge_Small);
 
             if (Trigger[0] == true)
             {
@@ -72,7 +82,7 @@ public class Respawn : MonoBehaviour
             {
                 for (int i = 0; i < Count_Medium; i++)
                 {
-                    GameObject Instant_Medium = Instantiate(Object_Medium[a], Return_RandomPosition(), Quaternion.identity);
+                    GameObject Instant_Medium = Instantiate(Object_Medium[b], Return_RandomPosition(), Quaternion.identity);
                     yield return new WaitForSeconds(1f);
                 }
                 Trigger[1] = false;
@@ -81,10 +91,27 @@ public class Respawn : MonoBehaviour
             {
                 for (int i = 0; i < Count_Small; i++)
                 {
-                    GameObject Instant_Small = Instantiate(Object_Small[a], Return_RandomPosition(), Quaternion.identity);
+                    GameObject Instant_Small = Instantiate(Object_Small[c], Return_RandomPosition(), Quaternion.identity);
                     yield return new WaitForSeconds(1f);
                 }
                 Trigger[2] = false;
+            }
+        }
+    }
+    void Making_Map(int[,] MAP)
+    {
+        for (int i = 0; i < range_Z; i++)
+        {
+            for (int j = 0; j < range_X; j++)
+            {
+                if( j == 0 || j == (range_X - 1) || i == 0 || i == (range_Z - 1))
+                {
+                    MAP[i, j] = 1;
+                }
+                else
+                {
+                    MAP[i, j] = 0;
+                }
             }
         }
     }
